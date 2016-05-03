ff9cd3e51a2a9b8feafe09e09678b21416cd2d04
Dave Shaffer <dave.shaffer@gmail.com>
2016-05-02 21:43:00 -0400

    Update carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     provider:'AWS',
-    region: 'us-standard',
+    region: 'us-west-2',
   }
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   config.fog_directory = ENV['FOG_DIRECTORY']
