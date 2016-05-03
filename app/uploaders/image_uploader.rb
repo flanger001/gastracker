009a7c104e600e3329f5ebcb6dd51baaaa9fba7a
Dave Shaffer <dave.shaffer@gmail.com>
2016-05-02 22:00:08 -0400

    Update image uploader and carrierwave initializer

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  storage :fog
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    provider:'AWS',
-    region: 'us-west-2',
+    provider: 'AWS',
+    region: 'us-west-2'
   }
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   config.fog_directory = ENV['FOG_DIRECTORY']
-  config.fog_public = false
+  config.fog_public = true
 end
