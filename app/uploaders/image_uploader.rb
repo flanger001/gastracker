2f1b1d2a76ecef50358e8b22a62435917c85b1d8
Dave Shaffer <dave.shaffer@gmail.com>
2016-05-02 22:47:35 -0400

    Try again with carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
   storage :fog
-  def extension_whitelist
+  def extension_white_list
     %w(jpg jpeg gif png)
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,11 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     provider: 'AWS',
-    region: 'us-west-2'
+    region: 'us-west-2',
+    host: ENV['FOG_HOST']
   }
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   config.fog_directory = ENV['FOG_DIRECTORY']
-  config.fog_public = true
+  config.fog_public = false
 end
