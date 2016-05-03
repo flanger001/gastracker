b336d722b78efcca1ac444b122d8f4b40c5f3281
Dave Shaffer <dave.shaffer@gmail.com>
2016-05-02 23:00:43 -0400

    One more time with feeling, again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     provider: 'AWS',
-    region: 'us-west-2',
-    host: ENV['FOG_HOST']
+    # region: 'us-west-2',
+    # host: ENV['FOG_HOST']
   }
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
