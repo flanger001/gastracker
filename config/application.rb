184ee4ca9ee74365956be0b8c37d8bacb31c4b56
David Shaffer <dave.shaffer@gmail.com>
2019-04-09 11:10:35 -0400

    Update Rails to 5.1.7

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,13 @@ Bundler.require(*Rails.groups)
 
 module GasTracker
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=172800"
+      "Cache-Control" => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "localhost:3019" }
 
