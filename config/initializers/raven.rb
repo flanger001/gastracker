e24e074d917d9078ba7b15d5c9720da73a9d498c
David Shaffer <dave.shaffer@gmail.com>
2019-04-11 19:15:44 -0400

    Rubocop Style/PercentLiteralDelimiters

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
   config.dsn = "https://#{ENV.fetch("SENTRY_USERNAME")}:#{ENV.fetch("SENTRY_PASSWORD")}@sentry.io/#{ENV.fetch("SENTRY_PROJECT_ID")}"
-  config.environments = %w(production)
+  config.environments = %w[production]
   config.ssl_verification = false
 end
