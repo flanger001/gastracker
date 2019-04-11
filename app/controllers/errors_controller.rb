8adede72f63b2958e1051b79b2c96bd3e06ee262
David Shaffer <dave.shaffer@gmail.com>
2019-04-11 15:25:23 -0400

    Delete ErrorMailer, report exceptions to Raven in ErrorsController

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,16 @@
 class ErrorsController < ApplicationController
   def render(*args, &block)
+    Raven.capture_exception(request.env["action_dispatch.exception"])
     super "error", *args, &block
   end
 
   def not_found
     @message = "The resource you are trying to reach cannot be found."
-    ErrorMailer.notify_admin(request, 404, "not found", current_user).deliver_now
     render :status => 404
   end
 
   def internal_server_error
     @message = "There was an error processing this request."
-    ErrorMailer.notify_admin(request, 500, "internal server error", current_user).deliver_now
     render :status => 500
   end
 end
