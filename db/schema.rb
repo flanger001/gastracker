f436e34c0d6930fd9af817a86fc19c7687f91062
David Shaffer <dave.shaffer@gmail.com>
2019-04-11 17:57:59 -0400

    Add default=false to user station favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_210638) do
+ActiveRecord::Schema.define(version: 2019_04_11_215104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2019_04_11_210638) do
   create_table "user_stations", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "station_id"
-    t.boolean "favorite"
+    t.boolean "favorite", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["station_id"], name: "index_user_stations_on_station_id"
