23c0e0d2e3864e0154d17d032d087e81184b4aa7
David Shaffer <dave.shaffer@gmail.com>
2019-04-13 02:38:26 -0400

    Prevent password request form from blowing up if user does not exist

--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -1,39 +1,56 @@
 require "rails_helper"
 
 RSpec.describe "User resets password" do
-  let!(:user) { create(:user) }
 
-  it "allows the user to reset their password" do
-    visit(new_password_request_path)
+  context "given an existing user" do
+    let!(:user) { create(:user) }
 
-    within("form") do
-      fill_in("Email", :with => user.email)
+    it "allows the user to reset their password" do
+      visit(new_password_request_path)
+
+      expect(page).to have_text("If you have an account, enter your email here and we will send you a password reset link shortly!")
+
+      within("form") do
+        fill_in("Email", :with => user.email)
+      end
+
+      click_button("Request new password")
+
+      visit(new_password_reset_path(:token => user.password_request.token))
+
+      expect(page).to have_text("Please enter your new password now. You will be logged out and can log back in with your new password.")
+
+      within("form") do
+        fill_in "password_reset[new_password]", :with => "friendship"
+        fill_in "password_reset[new_password_confirmation]", :with => "friendship"
+      end
+
+      click_button("Reset password")
+
+      visit(login_path)
+
+      within("form") do
+        fill_in "session[email]", :with => user.email
+        fill_in "session[password]", :with => "friendship"
+      end
+
+      click_button("Log In")
+
+      expect(page.current_path).to eq(dashboard_path)
     end
+  end
 
-    expect(page).to have_text("If you have an account, enter your email here and we will send you a password reset link shortly!")
+  context "given a non-existent user" do
+    it "does not fail to fill out the form but does not generate a token" do
+      visit(new_password_request_path)
 
-    click_button("Request new password")
+      within("form") do
+        fill_in("Email", :with => "user@example.com")
+      end
 
-    visit(new_password_reset_path(:token => user.password_request.token))
+      click_button("Request new password")
 
-    expect(page).to have_text("Please enter your new password now. You will be logged out and can log back in with your new password.")
-
-    within("form") do
-      fill_in "password_reset[new_password]", :with => "friendship"
-      fill_in "password_reset[new_password_confirmation]", :with => "friendship"
+      expect(page).to have_text("Please check your email for a password reset token.")
     end
-
-    click_button("Reset password")
-
-    visit(login_path)
-
-    within("form") do
-      fill_in "session[email]", :with => user.email
-      fill_in "session[password]", :with => "friendship"
-    end
-
-    click_button("Log In")
-
-    expect(page.current_path).to eq(dashboard_path)
   end
 end
